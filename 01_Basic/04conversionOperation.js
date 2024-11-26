de63f79b5782be2ee951c1e372570cf20d6c6b55
Sovagya-mallick <163491392+Sovagya-mallick@users.noreply.github.com>
2024-11-26 16:59:04 +0000

    conversion of Dtatypes

---
+++ 01_Basic/04conversionOperation.js
@@ -0,0 +1,24 @@
+let n = 23
+
+let num = "33"
+let num1 = "732ab"
+let num2 = null
+let num3 = undefined
+let numBoolean = true
+let numBoolean2 = false
+
+let numConvert = Number(num) //33 , type = number
+let numConvert1 = Number(num1) //NaN , type = number
+let numConvert2 = Number(num2) //0
+let numConvert3 = Number(num3) //NaN
+let numBooleanConvert = Number(numBoolean) //1
+let numBooleanConvert2 = Number(numBoolean2) //0
+
+
+
+let bool1 = 1
+let bool2 = 2
+
+let convertToBool1 = Boolean(bool1) //true
+let convertToBool2 = Boolean(bool2) // false
+// null => false
