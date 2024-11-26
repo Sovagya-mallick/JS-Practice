507475b8c54af65f9462590ac31f0bf2f1ec0c54
Sovagya-mallick <163491392+Sovagya-mallick@users.noreply.github.com>
2024-11-26 16:33:12 +0000

    Datatypes

---
+++ 01_Basic/00Datatypes.js
@@ -0,0 +1,17 @@
+//alert(4+4) // not alowed here, only in web version suported machine
+
+let name = "souvagya" //string
+let age = 32 //number => 2 to the power 53
+let isMale = true //boolean
+//bigint
+//null
+//undefined
+//symbol => unique (mostly used in implementation of react)
+
+
+//object
+
+//console.log(typeof null);
+
+// console.log(typeof undefined);
+ 
