75a8ec69abb3db6fd4da09225a961148d1e8392d
Sovagya-mallick <163491392+Sovagya-mallick@users.noreply.github.com>
2024-11-27 20:27:57 +0000

    comparision

---
+++ 01_Basic/05Comparision.js
@@ -0,0 +1,17 @@
+// console.log(2>2);
+// console.log(2!=4);
+// console.log(2>=5);
+// console.log(2=="2"); //true
+// console.log(2==="2"); //true (strict checking for datatypes)
+
+
+// console.log(null>0); // false (because here null is NaN)
+// console.log(null==0); //false (because here null is NaN)
+// console.log(null<=0); // true (Because here due to (=>) null treated as 0)
+// console.log(null>=0); // true (Because here due to (=>) null treated as 0)
+
+// console.log(undefined>0); // false 
+// console.log(undefined<0); // false
+// console.log(undefined==0); // false
+
+
